bf455ad774a124e79d8f1c501621a48f967f3868
chenxiaowen <18679850773@163.com>
2022-07-15 18:23:54 +0800

    updata

--- build/webpack.config.prod.js
+++ build/webpack.config.prod.js
@@ -1,58 +1,58 @@
-// const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const htmlWebpackPlugins = require('html-webpack-plugin')
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+// const htmlWebpackPlugins = require('html-webpack-plugin')
+// const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
     mode: 'production',
     entry: './src/main.js',
-    output: {
-        path: path.join(__dirname, './dist'),
-        filename: 'bundle.js'
-    },
+    // output: {
+    //     path: path.join(__dirname, './dist'),
+    //     filename: 'bundle.js'
+    // },
     module: {
-        rules: [
-            //配置处理.css文件第三方loader规则
-            {
-                test: /\.css/,
-                use: ['style-loader', 'css-loader'],
-                exclude: /node_modules/,
+        // rules: [
+        //     //配置处理.css文件第三方loader规则
+        //     {
+        //         test: /\.css/,
+        //         use: ['style-loader', 'css-loader'],
+        //         exclude: /node_modules/,
 
-            },
+        //     },
 
-            {
-                test: /\.(jpg|png|gif|jpeg|bmp)$/,
-                use: 'url-loader?limit=12170&name=[hash:8]-[name].[ext]',
+        //     {
+        //         test: /\.(jpg|png|gif|jpeg|bmp)$/,
+        //         use: 'url-loader?limit=12170&name=[hash:8]-[name].[ext]',
 
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: ['@babel/plugin-proposal-class-properties']
-                    }
-                }
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            }
-        ]
+        //     },
+        //     {
+        //         test: /\.js$/,
+        //         exclude: /node_modules/,
+        //         use: {
+        //             loader: 'babel-loader',
+        //             options: {
+        //                 presets: ['@babel/preset-env'],
+        //                 plugins: ['@babel/plugin-proposal-class-properties']
+        //             }
+        //         }
+        //     },
+        //     {
+        //         test: /\.vue$/,
+        //         loader: 'vue-loader'
+        //     }
+        // ]
     },
     plugins: [
         new CleanWebpackPlugin(),
-        new htmlWebpackPlugins({
-            template: path.join(__dirname, './src/index.html'),
-            filename: 'index.html'
-        }),
+        // new htmlWebpackPlugins({
+        //     template: path.join(__dirname, './src/index.html'),
+        //     filename: 'index.html'
+        // }),
         //请确保引入这个插件！
-        new VueLoaderPlugin(),
-        // new CopyPlugin([
-        //     { from: './src/', to: './' }
-        // ]),
+        // new VueLoaderPlugin(),
+        new CopyPlugin([
+            { from: './src/', to: './' }
+        ]),
     ],
 }
